a34255dd1b46465e76754fbd2365c0f0785f944e
Manas-19 <68516915+Manas-19@users.noreply.github.com>
2020-07-20 02:14:48 +0530

    Add files via upload

---
+++ pro/Assets/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject Sc;
+    private void GroundSpawn()
+    {
+        Instantiate(Sc);
+    }
+
+void OnTriggerEnter(Collider other)
+    {
+        GroundSpawn();
+    }
+}
